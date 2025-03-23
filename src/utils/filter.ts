96986b3a979216b509a95f7ab430d208e60277b8
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2025-03-24 00:21:54 +0800

    fix: text filter lowercase

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Ender-Wiggin
+ * @Date: 2025-03-07 23:02:36
+ * @LastEditors: Ender-Wiggin
+ * @LastEditTime: 2025-03-24 00:21:16
+ * @Description:
+ */
 import BaseCard from '@/types/BaseCard';
 import { EEffectType, Effect } from '@/types/effect';
 
@@ -25,7 +32,8 @@ const effectsHasText = (
   t: (text: string) => string
 ) => {
   if (!effects) return false;
-  const effectsStr = JSON.stringify(serializeEffects(effects, t));
+  // const effectsStr = JSON.stringify(serializeEffects(effects, t)).toLowerCase();
+  const effectsStr = effects2String(effects, t).toLowerCase();
   if (effectsStr.includes(text)) {
     return true;
   }
@@ -62,8 +70,45 @@ const serializeEffect = (effect: Effect, t: (text: string) => string) => {
         reward: serializeEffects(mission.reward, t),
       };
     });
+    effectObj.desc = effect.desc || '';
   } else if (effect.effectType === EEffectType.OR) {
     effectObj.effects = serializeEffects(effect.effects, t);
   }
   return effectObj;
+};
+
+const effects2String = (
+  effects: Effect | Effect[],
+  t: (text: string) => string
+): string => {
+  if (Array.isArray(effects)) {
+    const arr = effects.map((effect) => effect2String(effect, t));
+    return arr.join(' ');
+  } else {
+    return effect2String(effects, t);
+  }
+};
+
+const effect2String = (effect: Effect, t: (text: string) => string): string => {
+  let effectArray = [];
+  if (
+    effect.effectType === EEffectType.BASE ||
+    effect.effectType === EEffectType.CUSTOMIZED
+  ) {
+    effectArray.push(effect.type || '');
+    effectArray.push(t(effect?.helperText || ''));
+    effectArray.push(t(effect?.desc || ''));
+  } else if (
+    effect.effectType === EEffectType.MISSION_QUICK ||
+    effect.effectType === EEffectType.MISSION_FULL
+  ) {
+    effect.missions.forEach((mission) => {
+      effectArray.push(effects2String(mission.req, t));
+      effectArray.push(effects2String(mission.reward, t));
+    });
+    effectArray.push(effect.desc || '');
+  } else if (effect.effectType === EEffectType.OR) {
+    effectArray.push(effects2String(effect.effects, t));
+  }
+  return effectArray.join(' ');
 };
