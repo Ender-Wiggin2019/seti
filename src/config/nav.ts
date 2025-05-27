52c18e809bc494ddf8aefb42ac323c6d208a8555
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2025-05-28 00:12:27 +0800

    fix: about page route

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -2,7 +2,7 @@
  * @Author: Ender-Wiggin
  * @Date: 2025-02-25 09:56:21
  * @LastEditors: Ender-Wiggin
- * @LastEditTime: 2025-05-27 23:50:54
+ * @LastEditTime: 2025-05-28 00:12:08
  * @Description:
  */
 export const navigationItems = [
@@ -15,5 +15,5 @@ export const navigationItems = [
   // { href: '/quizzes', text: 'nav.daily_quiz' },
   // { href: '/cards', text: 'Cards' },
   // { href: '/actions', text: 'Actions' },
-  { href: '/post/about', text: 'nav.about' },
+  { href: '/posts/about', text: 'nav.about' },
 ];
